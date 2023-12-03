2c918e9c546c04d9eba66613691a620792a648f7
vasimalai05 <149891184+vasimalai05@users.noreply.github.com>
2023-12-03 10:43:24 +0530

    Update info.py

--- info.py
+++ info.py
@@ -57,7 +57,7 @@ SHORT_URL = environ.get("SHORT_URL")
 SHORT_API = environ.get("SHORT_API")
 
 # Others
-IMDB_DELET_TIME = int(environ.get('IMDB_DELET_TIME', "300"))
+IMDB_DELET_TIME = int(environ.get('IMDB_DELET_TIME', "1000"))
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', 0))
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'Kumaresan1991')
 P_TTI_SHOW_OFF = is_enabled(environ.get('P_TTI_SHOW_OFF', "True"), True)
